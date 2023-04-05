a1b187a0748e3567735d6cb5fcb36f9b2662666b
Tan Tai <taib1910442@student.ctu.edu.vn>
2023-04-05 23:00:56 +0700

    Cai dat cac handle truy xuat csdl

--- app/config/index.js
+++ app/config/index.js
@@ -3,7 +3,7 @@ const config = {
         port: process.env.PORT || 3000,
     },
     db: {
-        uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactbook"
+        uri: process.env.MONGODB_URI || "mongodb+srv://trantantai00931:th281202@contactbook.vzipmhh.mongodb.net/?retryWrites=true&w=majority"
     }
 }
 
